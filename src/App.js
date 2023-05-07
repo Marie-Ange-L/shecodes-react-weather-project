318899e5241ce05b7b598bf5615792006bae85bf
Marie-Ange-L <76390117+Marie-Ange-L@users.noreply.github.com>
2023-05-07 12:31:25 +0200

    HTML setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React from "react";
+import Weather from "./Weather";
+
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 export default function App() {
 	return (
 		<div className="App">
-			<h1>Weather App</h1>
-			<footer>
-				Coded by{" "}
-				<a
-					href="https://www.linkedin.com/in/marie-ange-lhernould/"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Marie-Ange L.
-				</a>{" "}
-				as part of her training with SheCodes &ndash; available on{" "}
-				<a
-					href="https://github.com/Marie-Ange-L/shecodes-react-weather-project"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</footer>
+			<div className="container">
+				<Weather />
+				<footer>
+					Coded by{" "}
+					<a
+						href="https://www.linkedin.com/in/marie-ange-lhernould/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Marie-Ange L.
+					</a>{" "}
+					as part of her training with SheCodes &ndash; available on{" "}
+					<a
+						href="https://github.com/Marie-Ange-L/shecodes-react-weather-project"
+						target="_blank"
+						rel="noreferrer"
+					>
+						GitHub
+					</a>
+				</footer>
+			</div>
 		</div>
 	);
 }
